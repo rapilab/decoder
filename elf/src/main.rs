a5da0c390c477200fdfbb10482d13a9fc2af8e0c
Phodal Huang <h@phodal.com>
2020-07-20 21:14:32 +0800

    feat: add simple demo for goblin

--- elf/src/main.rs
+++ elf/src/main.rs
@@ -1,3 +1,38 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+    use std::fs;
+    use goblin::{Object, error};
+
+    #[test]
+    fn test_parser_files() {
+        let path = Path::new("../_fixtures/c/hello");
+        parse_buffer(path);
+        assert_eq!(true, true)
+    }
+
+    fn parse_buffer(path: &Path) -> error::Result<()> {
+        let buffer = fs::read(path)?;
+        match Object::parse(&buffer)? {
+            Object::Elf(elf) => {
+                println!("elf: {:#?}", &elf);
+            },
+            Object::PE(pe) => {
+                println!("pe: {:#?}", &pe);
+            },
+            Object::Mach(mach) => {
+                println!("mach: {:#?}", &mach);
+            },
+            Object::Archive(archive) => {
+                println!("archive: {:#?}", &archive);
+            },
+            Object::Unknown(magic) => { println!("unknown magic: {:#x}", magic) }
+        }
+        Ok(())
+    }
+}
