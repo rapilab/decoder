5a3ce36e2abdd430ba7f92687e27e31756b24347
Phodal Huang <h@phodal.com>
2020-07-20 21:27:56 +0800

    refactor: move parse buffer to func

--- elf/src/main.rs
+++ elf/src/main.rs
@@ -1,3 +1,27 @@
+use std::path::Path;
+use std::fs;
+use goblin::{Object, error};
+
+pub fn parse_buffer(path: &Path) -> error::Result<()> {
+    let buffer = fs::read(path)?;
+    match Object::parse(&buffer)? {
+        Object::Elf(elf) => {
+            println!("elf: {:#?}", &elf);
+        }
+        Object::PE(pe) => {
+            println!("pe: {:#?}", &pe);
+        }
+        Object::Mach(mach) => {
+            println!("mach: {:#?}", &mach);
+        }
+        Object::Archive(archive) => {
+            println!("archive: {:#?}", &archive);
+        }
+        Object::Unknown(magic) => { println!("unknown magic: {:#x}", magic) }
+    }
+    Ok(())
+}
+
 fn main() {
     println!("Hello, world!");
 }
@@ -6,33 +30,12 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use std::path::Path;
-    use std::fs;
-    use goblin::{Object, error};
+    use crate::parse_buffer;
 
     #[test]
     fn test_parser_files() {
         let path = Path::new("../_fixtures/c/hello");
         parse_buffer(path);
         assert_eq!(true, true)
-    }
-
-    fn parse_buffer(path: &Path) -> error::Result<()> {
-        let buffer = fs::read(path)?;
-        match Object::parse(&buffer)? {
-            Object::Elf(elf) => {
-                println!("elf: {:#?}", &elf);
-            },
-            Object::PE(pe) => {
-                println!("pe: {:#?}", &pe);
-            },
-            Object::Mach(mach) => {
-                println!("mach: {:#?}", &mach);
-            },
-            Object::Archive(archive) => {
-                println!("archive: {:#?}", &archive);
-            },
-            Object::Unknown(magic) => { println!("unknown magic: {:#x}", magic) }
-        }
-        Ok(())
     }
 }
