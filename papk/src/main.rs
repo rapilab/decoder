50747b97da17e890cba266072ed70713e045ce33
Phodal Huang <h@phodal.com>
2020-07-21 21:03:07 +0800

    feat: add apk resources parse sample

--- papk/src/main.rs
+++ papk/src/main.rs
@@ -1,49 +1,98 @@
-use std::fs;
-use std::io::BufReader;
+extern crate abxml;
+extern crate zip;
+#[macro_use]
+extern crate failure;
+#[macro_use]
+extern crate log;
+
+use std::io::{BufReader, Read, Cursor};
+use abxml::visitor::{ModelVisitor, Executor, XmlVisitor, Resources};
+use abxml::encoder::Xml;
+use failure::{bail, Error, ResultExt};
 
 fn main() {
     println!("Hello, world!");
 }
 
-pub fn parse_apk(fname: String) {
-    let file = fs::File::open(&fname).unwrap();
-    let reader = BufReader::new(file);
+pub fn parse_apk(apk_path: String, target_file: String) -> Result<(), Error> {
+    let android_resources_content = abxml::STR_ARSC.to_owned();
 
-    let mut archive = zip::ZipArchive::new(reader).unwrap();
+    // APK
+    let file = std::fs::File::open(&apk_path)?;
+    let mut archive = zip::ZipArchive::new(file).unwrap();
+
+    let mut resources_content = Vec::new();
+    archive
+        .by_name("resources.arsc")
+        .unwrap()
+        .read_to_end(&mut resources_content)?;
+
+    let mut resources_visitor = ModelVisitor::default();
+    Executor::arsc(&resources_content, &mut resources_visitor)?;
+    Executor::arsc(&android_resources_content, &mut resources_visitor)?;
+
     for i in 0..archive.len() {
-        let file = archive.by_index(i).unwrap();
-        let outpath = file.sanitized_name();
-        {
-            let comment = file.comment();
-            if !comment.is_empty() {
-                println!("Entry {} comment: {}", i, comment);
+        let mut current_file = archive.by_index(i).unwrap();
+
+        if current_file.name().contains(&target_file) {
+            {
+                println!("Current file: {}", current_file.name());
+                let mut xml_content = Vec::new();
+                current_file.read_to_end(&mut xml_content)?;
+                let new_content = xml_content.clone();
+
+                let resources = resources_visitor.get_resources();
+                let out =
+                    parse_xml(&new_content, resources).context("could not decode target file")?;
+                println!("{}", out);
             }
         }
-
-        if (&*file.name()).ends_with('/') {
-            println!(
-                "Entry {} is a directory with name \"{}\"",
-                i,
-                outpath.as_path().display()
-            );
-        } else {
-            println!(
-                "Entry {} is a file with name \"{}\" ({} bytes)",
-                i,
-                outpath.as_path().display(),
-                file.size()
-            );
-        }
     }
+
+    Ok(())
 }
 
+fn parse_xml<'a>(content: &[u8], resources: &'a Resources<'a>) -> Result<String, Error> {
+    let cursor = Cursor::new(content);
+    let mut visitor = XmlVisitor::new(resources);
+
+    Executor::xml(cursor, &mut visitor)?;
+
+    match *visitor.get_root() {
+        Some(ref root) => match *visitor.get_string_table() {
+            Some(_) => {
+                let res =
+                    Xml::encode(visitor.get_namespaces(), root).context("could note encode XML")?;
+                return Ok(res);
+            }
+            None => {
+                println!("No string table found");
+            }
+        },
+        None => {
+            println!("No root on target XML");
+        }
+    }
+
+    bail!("could not decode XML")
+}
 
 #[cfg(test)]
 mod tests {
     use crate::parse_apk;
+    use failure::Error;
 
     #[test]
     fn test_parse_apk_binary() {
-        parse_apk(String::from("../_fixtures/apk/app-release-unsigned.apk"));
+        let result = parse_apk(String::from("../_fixtures/apk/app-release-unsigned.apk"),
+                               String::from("standalone_badge.xml"));
+        match result {
+            Ok(_) => {
+                println!("zz")
+            }
+            Err(_) => {
+                println!("error")
+            }
+        }
     }
 }
