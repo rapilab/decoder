61bee31acd372d010e25e725ca049515fbd359f6
Phodal Huang <h@phodal.com>
2020-07-22 00:03:40 +0800

    feat: add class parse support

--- pclass/src/lib.rs
+++ pclass/src/lib.rs
@@ -1,3 +1,28 @@
-pub fn parse_class() {
+use pclass_parser::parse_class as pclass_parser;
+use pclass_parser::classfile::ClassFile;
 
+pub fn parse_class(input: &[u8]) -> nom::IResult<&[u8], ClassFile> {
+    pclass_parser(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+    use crate::parse_class;
+    use std::fs;
+    use pclass_parser::classfile::ClassFile;
+    use nom::error::ErrorKind;
+
+    #[test]
+    fn test_parse_class() {
+        let path = Path::new("../_fixtures/java/hello/HelloWorld.class");
+        let buffer = fs::read(path).unwrap();
+        let result = parse_class(buffer.as_ref());
+        match result {
+            Ok((_, class)) => {
+                assert_eq!(52, class.version.major)
+            },
+            Err(_) => {},
+        }
+    }
 }
