4e7665a5605aff9f17a3e2b0aea87987e7f23cbe
Phodal Huang <h@phodal.com>
2020-07-21 20:08:59 +0800

    feat: extract parse dex files

--- pdex/src/main.rs
+++ pdex/src/main.rs
@@ -1,29 +1,44 @@
 use std::io;
 use std::path::Path;
-use dex::DexReader;
+
+use dex::{Dex, DexReader, Error};
+use dex::class::Class;
+use memmap::Mmap;
 
 fn main() {
     println!("Hello, world!");
 }
 
-
-fn parse_dex(path: String) -> io::Result<()> {
+pub fn parse_dex(path: String) -> Option<Dex<Mmap>> {
     let file = Path::new(&path);
-    let dex = DexReader::from_file(file).unwrap();
-    let class = dex
-        .find_class_by_name("LHelloWorld;")
-        .expect("Failed to load class")
-        .expect("class not found");
-    println!("class type: {}", class.jtype());
-    Ok(())
+    let dex = DexReader::from_file(file);
+    match dex {
+        Ok(data) => {
+            Some(data)
+        }
+        Err(_) => {
+            None
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::parse_dex;
+    use dex::class::Class;
 
     #[test]
     fn test_parse_c_binary() {
-        parse_dex(String::from("../_fixtures/java/hello/classes.dex"));
+        let mmap = parse_dex(String::from("../_fixtures/java/hello/classes.dex"));
+        let result = mmap.unwrap().find_class_by_name("LHelloWorld;");
+
+        let option = result.unwrap();
+        match option {
+            None => {},
+            Some(class) => {
+                let str = class.source_file().unwrap();
+                assert_eq!("HelloWorld.java", &str.to_string())
+            },
+        }
     }
 }
